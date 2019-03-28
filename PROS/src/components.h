f2c1a362d5d957a50f438acdd37e781dbb99e734
srithon <36751023+srithon@users.noreply.github.com>
2019-03-28 10:37:43 -0400

    Renamed roller and roller2
    Now named flipperRoller and intakeRoller

--- PROS/src/components.h
+++ PROS/src/components.h
@@ -28,8 +28,9 @@ const extern pros::Motor* rightMF;// = new pros::Motor(RIGHT_MOTOR_FRONT_PORT, p
 const extern pros::Motor* leftMB;// = new pros::Motor(LEFT_MOTOR_BACK_PORT, pros::E_MOTOR_GEARSET_18, true, pros::E_MOTOR_ENCODER_DEGREES);
 const extern pros::Motor* rightMB;// = new pros::Motor(RIGHT_MOTOR_BACK_PORT, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
 const extern pros::Motor* shooter;// = new pros::Motor(SHOOTER_MOTOR_PORT, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
-const extern pros::Motor* roller;// = new pros::Motor(ROLLER_MOTOR_PORT, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
-const extern pros::Motor* roller2;
+const extern pros::Motor* flipperRoller;// = new pros::Motor(ROLLER_MOTOR_PORT, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
+const extern pros::Motor* intakeRoller;
+
 const extern pros::Motor* driveMotors[4];// = {leftMF, rightMF, leftMB, rightMB};
 
 //extern pros::Controller* controller;// = new pros::Controller(pros::E_CONTROLLER_MASTER);
